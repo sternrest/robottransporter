e092c40ee94c75be7181559840723567bbf0e2d0
Azzami Mutahhari Al Farrah <azzamimutahharialfarrah@gmail.com>
2024-10-16 11:58:17 +0700

    Update ESP32_Bluetooth_Car_Transporter.ino

--- ESP32_Bluetooth_Car_Transporter.ino
+++ ESP32_Bluetooth_Car_Transporter.ino
@@ -1,1 +1,149 @@
-w
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include <DabbleESP32.h>
+#include <ESP32Servo.h>
+
+//servo init
+int posA = 0, posB = 0;
+Servo servoA; Servo servoB;
+
+//Right motor
+int enableRightMotor=22; 
+int rightMotorPin1=16;
+int rightMotorPin2=17;
+
+//Left motor
+int enableLeftMotor=23;
+int leftMotorPin1=18;
+int leftMotorPin2=19;
+
+#define MAX_MOTOR_SPEED 255
+
+const int PWMFreq = 1000; /* 1 KHz */
+const int PWMResolution = 8;
+const int rightMotorPWMSpeedChannel = 4;
+const int leftMotorPWMSpeedChannel = 5;
+
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)
+{
+  if (rightMotorSpeed < 0)
+  {
+    digitalWrite(rightMotorPin1,LOW);
+    digitalWrite(rightMotorPin2,HIGH);    
+  }
+  else if (rightMotorSpeed > 0)
+  {
+    digitalWrite(rightMotorPin1,HIGH);
+    digitalWrite(rightMotorPin2,LOW);      
+  }
+  else
+  {
+    digitalWrite(rightMotorPin1,LOW);
+    digitalWrite(rightMotorPin2,LOW);      
+  }
+  
+  if (leftMotorSpeed < 0)
+  {
+    digitalWrite(leftMotorPin1,LOW);
+    digitalWrite(leftMotorPin2,HIGH);    
+  }
+  else if (leftMotorSpeed > 0)
+  {
+    digitalWrite(leftMotorPin1,HIGH);
+    digitalWrite(leftMotorPin2,LOW);      
+  }
+  else
+  {
+    digitalWrite(leftMotorPin1,LOW);
+    digitalWrite(leftMotorPin2,LOW);      
+  }
+  
+  ledcWrite(rightMotorPWMSpeedChannel, abs(rightMotorSpeed));
+  ledcWrite(leftMotorPWMSpeedChannel, abs(leftMotorSpeed));  
+}
+
+void setUpPinModes()
+{
+  servoA.attach(2);
+  servoB.attach(3);
+
+  pinMode(enableRightMotor,OUTPUT);
+  pinMode(rightMotorPin1,OUTPUT);
+  pinMode(rightMotorPin2,OUTPUT);
+  
+  pinMode(enableLeftMotor,OUTPUT);
+  pinMode(leftMotorPin1,OUTPUT);
+  pinMode(leftMotorPin2,OUTPUT);
+
+  //Set up PWM for speed
+  ledcAttach(rightMotorPWMSpeedChannel, PWMFreq, PWMResolution);
+  ledcAttach(leftMotorPWMSpeedChannel, PWMFreq, PWMResolution);  
+  //ledcAttachPin(enableRightMotor, rightMotorPWMSpeedChannel);
+  //ledcAttachPin(enableLeftMotor, leftMotorPWMSpeedChannel); 
+
+  rotateMotor(0,0); 
+  servoA.write(posA);
+  servoB.write(posB);
+}
+
+void setup()
+{
+  setUpPinModes();
+  Dabble.begin("MyBluetoothCar"); 
+}
+
+void loop()
+{
+  int rightMotorSpeed=0;
+  int leftMotorSpeed=0;
+  Dabble.processInput();
+  if (GamePad.isUpPressed())
+  {
+    rightMotorSpeed = MAX_MOTOR_SPEED;
+    leftMotorSpeed = MAX_MOTOR_SPEED;
+  }
+
+  if (GamePad.isDownPressed())
+  {
+    rightMotorSpeed = -MAX_MOTOR_SPEED;
+    leftMotorSpeed = -MAX_MOTOR_SPEED;
+  }
+
+  if (GamePad.isLeftPressed())
+  {
+    rightMotorSpeed = MAX_MOTOR_SPEED;
+    leftMotorSpeed = -MAX_MOTOR_SPEED;
+  }
+
+  if (GamePad.isRightPressed())
+  {
+    rightMotorSpeed = -MAX_MOTOR_SPEED;
+    leftMotorSpeed = MAX_MOTOR_SPEED;
+  }
+
+  rotateMotor(rightMotorSpeed, leftMotorSpeed);
+
+  if (GamePad.isTrianglePressed())
+  {
+    posA += 10;
+    servoA.write(posA);
+  }
+
+  if (GamePad.isCrossPressed())
+  {
+    posA -= 10;
+    servoA.write(posA);
+  }
+
+  if (GamePad.isCirclePressed())
+  {
+    posB += 10;
+    servoB.write(posB);
+  }
+
+  if (GamePad.isSquarePressed())
+  {
+    posB -= 10;
+    servoB.write(posB);
+  }  
+}
